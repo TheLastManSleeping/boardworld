9189d0345495527251b353ee8cf9ccd80da11c96
arter <arthur.khalipov@gmail.com>
2020-05-24 23:53:53 +0300

    Initialize Django project

--- SaveMe/settings.py
+++ SaveMe/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    ''
 
     'urls',
     'allauth',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
